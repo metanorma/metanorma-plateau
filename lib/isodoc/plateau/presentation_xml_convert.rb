97d26852c9f2ef1318f11a61d8354bc97767eb38
Nick Nicholas <opoudjis@gmail.com>
2024-05-24 01:37:55 +1000

    no middle title: https://github.com/metanorma/metanorma-plateau/issues/11

--- lib/isodoc/plateau/presentation_xml_convert.rb
+++ lib/isodoc/plateau/presentation_xml_convert.rb
@@ -3,11 +3,12 @@ require_relative "init"
 module IsoDoc
   module Plateau
     class PresentationXMLConvert < IsoDoc::JIS::PresentationXMLConvert
- def convert(input_filename, file = nil, debug = false,
-                output_filename = nil)
-   super
- end
+      def convert(input_filename, file = nil, debug = false,
+                     output_filename = nil)
+        super
+      end
 
+      def middle_title(docxml); end
 
       include Init
     end
