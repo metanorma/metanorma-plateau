dab91782b986b320697ed11bf360146399b71aed
Nick Nicholas <opoudjis@gmail.com>
2024-05-23 23:52:40 +1000

    annul JIS sections ordering with introduction first:  https://github.com/metanorma/metanorma-plateau/issues/12

--- lib/isodoc/plateau/xref.rb
+++ lib/isodoc/plateau/xref.rb
@@ -1,6 +1,11 @@
 module IsoDoc
   module Plateau
     class Xref < IsoDoc::JIS::Xref
+      def clause_order_main(docxml)
+        [
+          { path: "//sections/introduction | //sections/clause | //sections/terms | //sections/references", multi: true }
+        ]
+      end
     end
   end
 end
