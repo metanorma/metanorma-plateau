bf9949474351e5c4c7e4c7a37d6529fd6b4930c9
Nick Nicholas <opoudjis@gmail.com>
2025-05-07 00:02:30 +1000

    rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,6 +44,7 @@ end
 
 def strip_guid(xml)
   xml.gsub(%r{ id="_[^"]+"}, ' id="_"')
+    .gsub(%r{ semx-id="[^"]*"}, '')
     .gsub(%r{ original-id="_[^"]+"}, ' original-id="_"')
     .gsub(%r{ target="_[^"]+"}, ' target="_"')
     .gsub(%r{ target="_[^"]+"}, ' target="_"')
